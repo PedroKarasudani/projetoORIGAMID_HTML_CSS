4ad22ff1b5accce4a5b47b4b31509e66f752565b
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-02-16 22:20:23 -0300

    JS: Perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -24,3 +24,25 @@ function ativarProduto(parametro) {
 }
 
 url.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas dt button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativo");
+  const ativa = resposta.classList.contains("ativo");
+  pergunta.setAttribute("aria-expanded", ativa);
+
+  console.log(controls);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
