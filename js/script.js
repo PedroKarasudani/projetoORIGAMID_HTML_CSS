7a2020e5cabe5715d6effe3b4028f43dd533745f
Pedro K. L <pedro.karasudani@hotmail.com>
2024-02-16 21:28:28 -0300

    JS: parametros URL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//ativar link menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +11,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ativar itens orcamento
+
+const url = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+url.forEach(ativarProduto);
